311eaa399344fb2a4e0c2b99791d3e14d0b03f35
Atharva vadodkar <158564201+atharva-1304@users.noreply.github.com>
2024-07-26 13:04:20 +0530

    update js and add functions for scroll

--- app.js
+++ app.js
@@ -7,18 +7,32 @@ const brandImg = document.querySelector("#brandImg");
 const togglerIcon = document.querySelector("#toggler-icon");
 const navlinks =  document.querySelectorAll(".nav-link");
 
+function updateNavLinkClasses() {
+  navlinks.forEach(link => {
+    if (window.scrollY > 40) {
+      link.classList.add('navlinksScroll');
+      link.classList.remove('navlinks');
+    } else {
+      link.classList.add('navlinks');
+      link.classList.remove('navlinksScroll');
+    }
+  });
+}
+
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 40) {
     navbar.classList.add('navbarScroll');
     navbrand.style.color="black";
     brandImg.src='./images/navbarbrandscroll.png';
     togglerIcon.style.color="black";
-  
+    updateNavLinkClasses();
     
   }else if(window.scrollY < 40){
     navbar.classList.remove('navbarScroll');
     navbrand.style.color="white";
     brandImg.src='./images/navbarbrandtop.png';
     togglerIcon.style.color="white";
+    updateNavLinkClasses();
   }
 });
