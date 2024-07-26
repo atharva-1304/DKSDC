cc191511a33aa61c4e0fc12ca589601b88208a2a
atharva-1340 <atharvavadodkar@gmail.com>
2024-07-27 00:15:44 +0530

    adding new section

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // jay gajanan
-// navbar Script
 
+// navbar Script
 const navbar = document.querySelector(".navbar");
 const navbrand = document.querySelector(".navbar-brand");
 const brandImg = document.querySelector("#brandImg");
 const togglerIcon = document.querySelector("#toggler-icon");
 const navlinks =  document.querySelectorAll(".nav-link");
+const navbody  = document.querySelector("#nav-container");
 
 function updateNavLinkClasses() {
   navlinks.forEach(link => {
@@ -19,7 +20,6 @@ function updateNavLinkClasses() {
   });
 }
 
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 40) {
     navbar.classList.add('navbarScroll');
@@ -27,6 +27,8 @@ window.addEventListener("scroll", () => {
     brandImg.src='./images/navbarbrandscroll.png';
     togglerIcon.style.color="black";
     updateNavLinkClasses();
+    navbody.classList.remove('navbody');
+    navbody.classList.add('navbodyScroll');
     
   }else if(window.scrollY < 40){
     navbar.classList.remove('navbarScroll');
@@ -34,5 +36,9 @@ window.addEventListener("scroll", () => {
     brandImg.src='./images/navbarbrandtop.png';
     togglerIcon.style.color="white";
     updateNavLinkClasses();
+    navbody.classList.remove('navbodyScroll');
+    navbody.classList.add('navbody');
+
+
   }
 });
